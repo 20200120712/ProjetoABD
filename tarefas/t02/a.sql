cd73ce218bc3cf62140e271fa6044567d5541424
20200120712 <danielmedeiros200@gmail.com>
2023-05-10 19:56:54 -0300

    #2

--- tarefas/t02/a.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION calcular_idade(data_nasc DATE)
-RETURNS INTEGER AS $$
-DECLARE 
-    idade INTEGER;
-BEGIN
-    idade := EXTRACT(YEAR FROM age(now(), data_nasc));
-    RETURN idade;
-END;
-$$ LANGUAGE plpgsql;
