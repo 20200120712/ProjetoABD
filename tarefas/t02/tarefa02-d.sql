5ae3215f579013cf02b403f4cc5603a06dcabd16
20200120712 <danielmedeiros200@gmail.com>
2023-05-12 16:23:09 -0300

    editado #2

--- tarefas/t02/tarefa02-d.sql
+++ tarefas/t02/tarefa02-d.sql
@@ -1,9 +1,9 @@
-CREATE OR REPLACE FUNCTION atraso_atividade(data_limite DATE, data_conclusao DATE)
+CREATE OR REPLACE FUNCTION atraso_atividade(dataLimite DATE, dataConclusão DATE)
 RETURNS INTEGER AS $$
 DECLARE
   dias_atraso INTEGER;
 BEGIN
-  dias_atraso := EXTRACT(DAY FROM (data_conclusao - data_limite));
+  dias_atraso := EXTRACT(DAY FROM (dataConclusão - dataLimite));
   RETURN dias_atraso;
 EXCEPTION WHEN OTHERS THEN
   RETURN NULL;
