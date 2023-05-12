a22c0ca0e990ede534d1c094e291bd4ae0a65978
20200120712 <danielmedeiros200@gmail.com>
2023-05-12 17:44:20 -0300

    correção de erros e adaptação

--- tarefas/t02/tarefa02-e.sql
+++ tarefas/t02/tarefa02-e.sql
@@ -1,19 +1,23 @@
-CREATE OR REPLACE FUNCTION atraso_projeto(codProjeto integer)
-RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION atraso_projeto(cod_projeto integer)
+RETURNS integer
+LANGUAGE plpgsql
+AS $$
 DECLARE
-    data_conclusao_planejada date;
-    data_conclusao_real date;
-    dias_atraso integer;
+    data_fim_projeto date;
+    data_conclusao_projeto date;
+    qtde_dias_atraso integer;
 BEGIN
-    SELECT dataFim, dataConclusao INTO data_conclusao_planejada, data_conclusao_real
-    FROM projeto
-    WHERE codigo = codProjeto;
-    
-    IF data_conclusao_real IS NOT NULL THEN
-        dias_atraso := (data_conclusao_real - data_conclusao_planejada)::int;
-        RETURN dias_atraso;
-    ELSE
+    SELECT p.dataFim, p.dataConclusao INTO data_fim_projeto, data_conclusao_projeto
+    FROM projeto p
+    WHERE p.codigo = cod_projeto;
+
+    IF data_fim_projeto IS NULL THEN
         RETURN NULL;
+    ELSIF data_conclusao_projeto IS NULL OR data_conclusao_projeto <= data_fim_projeto THEN
+        RETURN 0;
+    ELSE
+        qtde_dias_atraso := (EXTRACT(EPOCH FROM data_conclusao_projeto - data_fim_projeto) / 86400)::integer;
+        RETURN qtde_dias_atraso;
     END IF;
 END;
-$$ LANGUAGE plpgsql;
+$$;
