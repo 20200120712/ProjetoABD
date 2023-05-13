7ddb3406167ae9a872a59a8baa60da5f60de1e7d
20200120712 <danielmedeiros200@gmail.com>
2023-05-13 09:28:48 -0300

    correção letra d #2

--- tarefas/t02/tarefa02-d.sql
+++ tarefas/t02/tarefa02-d.sql
@@ -1,11 +1,16 @@
-CREATE OR REPLACE FUNCTION atraso_atividade(dataLimite DATE, dataConclusão DATE)
-RETURNS INTEGER AS $$
+CREATE OR REPLACE FUNCTION dias_atraso(codAtividade int) RETURNS int
+AS $$
 DECLARE
-  dias_atraso INTEGER;
+  v_dataFim date;
+  v_dataConclusao date;
+  v_dias int;
 BEGIN
-  dias_atraso := EXTRACT(DAY FROM (dataConclusão - dataLimite));
-  RETURN dias_atraso;
-EXCEPTION WHEN OTHERS THEN
-  RETURN NULL;
+  SELECT a.dataFim, a.dataConclusao INTO v_dataFim, v_dataConclusao FROM atividade a WHERE a.codigo = codAtividade;
+  IF v_dataFim IS NOT NULL AND v_dataConclusao IS NULL THEN
+    v_dias := DATEDIFF(CURDATE(), v_dataFim);
+  ELSE
+    v_dias := 0;
+  END IF;
+  RETURN v_dias;
 END;
 $$ LANGUAGE plpgsql;
